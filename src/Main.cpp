95f6f441d6422e024d72cae854f183a6ce5f009f
VIktor Mandlbauer <viktormandlbauer@gmail.com>
2022-02-25 08:02:17 +0100

    Shifting text

--- src/Main.cpp
+++ src/Main.cpp
@@ -93,6 +93,8 @@ void setup()
   init(max7219_REG_shutdown, 0x01);
   // Not test mode
   init(max7219_REG_displayTest, 0x00);
+  // Set display
+  init(max7219_REG_intensity, 0x0F);
 
   clear_screen();
 }
@@ -213,21 +215,47 @@ void overlay(byte number, byte offset)
 
 void loop()
 {
-  overlay(0, 24);
-  overlay(1, 16);
-  overlay(2, 8);
-  overlay(3, 0);
+  for ( int i = 0; i < 64; i++ ){
+  
+  int pos_0 = 24 + i;
+  int pos_1 = 16 + i;
+  int pos_2 = 8 + i;
+  int pos_3 = 0 + i;
+  overlay(0, pos_0);
+  overlay(1, pos_1);
+  overlay(2, pos_2);
+  overlay(3, pos_3);
+  
+  if(i > 8){
+    int pos_4 = 24 + i - 32;
+    overlay(4, pos_4);
+    
+    if(i > 16){
+      int pos_5 = 16 + i - 32;
+      overlay(5, pos_5);
+
+      if(i > 24){
+        int pos_6 = 8 + i - 32;
+        overlay(6, pos_6);
+
+        if(i > 32){
+          int pos_7 = 0 + i - 32;
+          overlay(7, pos_7);
+        }
+      }
+    }
+  }
+
   draw_matrix_8x32();
-  delay(1000);
+  delay(100);
   clear_screen();
+  }
 
-
-  overlay(3, 24);
-  overlay(2, 16);
-  overlay(1, 8);
-  overlay(0, 0);
-  draw_matrix_8x32();
-  delay(1000);
-  clear_screen();
-
+  // overlay(3, 24);
+  // overlay(2, 16);
+  // overlay(1, 8);
+  // overlay(0, 0);
+  // draw_matrix_8x32();
+  // delay(1000);
+  // clear_screen();
 }
