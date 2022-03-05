fba349086dbdcd940e6fbabd51c80ccaf9741bb2
VIktor Mandlbauer <viktormandlbauer@gmail.com>
2022-03-05 01:01:01 +0100

    Positive and negative temperatures

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,5 +1,8 @@
 
 #include <Arduino.h>
+#include <stdlib.h>
+
+#define DATA_PIN 2
 
 void setup()
 {
@@ -12,7 +15,7 @@ int reset()
     uint8_t retries = 125;
 
     cli();
-    pinMode(10, INPUT);
+    pinMode(DATA_PIN, INPUT);
     sei();
 
     do
@@ -20,22 +23,22 @@ int reset()
         if (--retries == 0)
             return 1;
         delayMicroseconds(2);
-    } while (!digitalRead(10));
+    } while (!digitalRead(DATA_PIN));
 
     // Master pulls low for 480us
     cli();
-    digitalWrite(10, LOW);
-    pinMode(10, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
+    pinMode(DATA_PIN, OUTPUT);
     sei();
     delayMicroseconds(480);
 
     // Release and wait for slave to respond
     cli();
-    pinMode(10, INPUT);
+    pinMode(DATA_PIN, INPUT);
     delayMicroseconds(70);
 
     // Slave pulls low for 60 - 240us
-    r = !digitalRead(10);
+    r = !digitalRead(DATA_PIN);
     sei();
 
     // Wait for Pullup
@@ -48,10 +51,10 @@ void write_1()
 {
 
     cli();
-    digitalWrite(10, LOW);
-    pinMode(10, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
+    pinMode(DATA_PIN, OUTPUT);
     delayMicroseconds(5);
-    pinMode(10, INPUT);
+    pinMode(DATA_PIN, INPUT);
     delayMicroseconds(80);
     sei();
 }
@@ -59,24 +62,25 @@ void write_1()
 void write_0()
 {
     cli();
-    digitalWrite(10, LOW);
-    pinMode(10, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
+    pinMode(DATA_PIN, OUTPUT);
     delayMicroseconds(80);
-    pinMode(10, INPUT);
+    pinMode(DATA_PIN, INPUT);
     delayMicroseconds(5);
     sei();
 }
 
-int read(){
+int read()
+{
     cli();
-    digitalWrite(10, LOW);
-    pinMode(10, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
+    pinMode(DATA_PIN, OUTPUT);
     delayMicroseconds(5);
-    pinMode(10, INPUT);
-    delayMicroseconds(10);
+    pinMode(DATA_PIN, INPUT);
+    delayMicroseconds(15);
     sei();
 
-    return digitalRead(10);
+    return digitalRead(DATA_PIN);
 }
 
 void skip_rom()
@@ -92,78 +96,212 @@ void skip_rom()
     write_1();
 }
 
-void convertT(){
+void send_byte(byte byte)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        if (byte & (0x01 << i))
+        {
+            write_1();
+        }
+        else
+        {
+            write_0();
+        }
+    }
+}
 
-    // 44
-    write_0();
-    write_0();
-    write_1();
-    write_0();
+void send_64bitcode(byte *bytes)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        send_byte(bytes[i]);
+    }
+}
 
-    write_0();
-    write_0();
-    write_1();
-    write_0();
+void read_rom()
+{
+    send_byte(0x33);
+}
 
+void convertT()
+{
+    send_byte(0x44);
+    while (!read())
+        ;
 }
 
 void read_scratchpad()
 {
-    // BE
-
-    write_0();
-    write_1();
-    write_1();
-    write_1();
-
-    write_1();
-    write_1();
-    write_0();
-    write_1();
+    send_byte(0xBE);
 }
 
+void match_rom()
+{
+    send_byte(0x55);
+}
 
-
-void loop()
+byte *getROM()
 {
 
-    byte scratchpad[9] = {
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000};
+    static byte rom[8] = {
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+
+    int byte_array_index = 0;
+    int mod_8;
+    byte mask;
+
+    reset();
+    read_rom();
+
+    for (int i = 0; i < 64; i++)
+    {
+        mod_8 = i % 8;
+
+        if (mod_8 == 0)
+        {
+            byte_array_index++;
+        }
+        if (read())
+        {
+            mask = 0x1 << mod_8;
+            rom[byte_array_index - 1] |= mask;
+        }
+    }
+
+    return rom;
+}
+
+byte *getScratchpad()
+{
+
+    static byte scratchpad[9] = {
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 
     reset();
     skip_rom();
     read_scratchpad();
-    
-    int byte_array_index = 0;
-    byte mod_8;
+
+    int byte_array_index = 9;
+    int mod_8;
     byte mask;
 
     for (int i = 0; i < 72; i++)
     {
         mod_8 = i % 8;
-        
-        if(mod_8 == 0){
-            byte_array_index ++;
+
+        if (mod_8 == 0)
+        {
+            byte_array_index--;
         }
-        if(read()){
-            mask = 0x1 << (7 - mod_8);
+        if (read())
+        {
+            mask = 0x1 << mod_8;
+            scratchpad[byte_array_index] |= mask;
+        }
+    }
+    return scratchpad;
+}
+
+byte *getScratchpad(byte *rom)
+{
+
+    static byte scratchpad[9] = {
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+
+    reset();
+    match_rom();
+    send_64bitcode(rom);
+    read_scratchpad();
+
+    int byte_array_index = 0;
+    int mod_8;
+    byte mask;
+
+    for (int i = 0; i < 72; i++)
+    {
+        mod_8 = i % 8;
+
+        if (mod_8 == 0)
+        {
+            byte_array_index++;
+        }
+        if (read())
+        {
+            mask = 0x1 << mod_8;
             scratchpad[byte_array_index - 1] |= mask;
         }
     }
+    return scratchpad;
+}
 
-    for(int i = 0; i <= 8; i++){
-        Serial.print(scratchpad[i], BIN);
-        Serial.print("\n");
+byte read_byte()
+{
+    byte read_byte;
+    for (int i = 0; i < 8; i++)
+    {
+        if (read())
+        {
+            read_byte |= 0x01 << i;
+        }
     }
-    Serial.print("\n");
+    return read_byte;
+}
 
-    delay(5000);
+int getTemperature()
+{
+
+    double temperatur = 0;
+
+    reset();
+    skip_rom();
+    read_scratchpad();
+
+    byte lsb = read_byte();
+    byte msb = read_byte();
+
+    uint16_t two_bytes = (msb << 8) | lsb;
+
+    // Negative Temperatur
+    if (msb & 0b111100)
+    {
+        two_bytes = two_bytes xor 0xFFF0;
+        for (int i = -4; i < 6; i++){
+            if (two_bytes & (0x01 << (i + 4)))
+            {
+                if(i >= 0){
+                    temperatur += pow(2, i);
+                }else{
+                    temperatur -= pow(2, i);
+                }
+            }
+        }
+        return (temperatur + 1) * -1;
+    }
+    
+    // Positive Temperatur
+    for (int i = -4; i < 6; i++)
+    {
+        if (two_bytes & (0x01 << (i + 4)))
+        {
+            temperatur += pow(2, i);
+        }
+    }
+    return temperatur;
+}
+
+void loop()
+{
+    byte *rom = getROM();
+
+    reset();
+    skip_rom();
+    delay(10);
+    convertT();
+    delay(10);
+
+    getTemperature();
+
+    delay(500);
+    Serial.println();
 }
